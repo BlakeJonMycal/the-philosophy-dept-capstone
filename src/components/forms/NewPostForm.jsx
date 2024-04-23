084fbb010e362b0cf3ced2b770508eb53187486c
Blake Jon Mycal Smith <B.jonmycal@gmail.com>
2024-04-23 15:43:21 -0400

    updated main

---
+++ src/components/forms/NewPostForm.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react"
+import "./form.css"
+
+export const NewPostForm = () => {
+    const [newPost, setNewPost] = useState({})
+    const [schoolType, setSchoolType] = useState([])
+    
+
+}
